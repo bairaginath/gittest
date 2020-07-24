f35b10f7a13b2da7f4f03eb8a64e218a57633ae9
bairaginath <bairaginath2013@gmail.com>
2020-07-24 15:31:24 +0530

    iss53 modified

--- MyTest.java
+++ MyTest.java
@@ -2,7 +2,7 @@ public class MyTest {
 
 
 public static void main(){
-  System.out.println("Hello World Git with new branch");
+  System.out.println("Hello World Git with new branch,modifed");
 }
 
 }
