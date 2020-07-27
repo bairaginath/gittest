cd4e1d46c258a9beaff5a7cebe442f2b843b1408
bairaginath <bairaginath2013@gmail.com>
2020-07-27 17:01:20 +0530

    fetch and merge example

--- MyTest.java
+++ MyTest.java
@@ -5,6 +5,7 @@ public static void main(){
   System.out.println("Hello World Git with new branch,modifed with forking repo");
   System.out.println("change in my forked repo");
   System.out.println("another persion changed");
+  System.out.println("fetch and merge example");
 }
 
 }
