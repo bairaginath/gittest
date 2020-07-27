a0bb3e0ae6ec507cb56b77237237320d30f7d2e1
jamesindia <bairagi.nath@gmail.com>
2020-07-27 13:07:11 +0530

    change in forking repo

--- MyTest.java
+++ MyTest.java
@@ -3,6 +3,7 @@ public class MyTest {
 
 public static void main(){
   System.out.println("Hello World Git with new branch,modifed with forking repo");
+  System.out.println("change in my forked repo");
 }
 
 }
