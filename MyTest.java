7420f12cfa2eae729e7d26ade2a06c25906395dc
bairaginath <bairaginath2013@gmail.com>
2020-07-27 16:02:12 +0530

    change in main master branch

--- MyTest.java
+++ MyTest.java
@@ -4,6 +4,7 @@ public class MyTest {
 public static void main(){
   System.out.println("Hello World Git with new branch,modifed with forking repo");
   System.out.println("change in my forked repo");
+  System.out.println("another persion changed");
 }
 
 }
